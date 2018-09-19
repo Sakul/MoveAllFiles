22c53765b008b34c46ea35adad8cdc8f313c1aa8
sakul <miolynet@perfenterprise.com>
2018-09-20 06:03:27 +0700

    Clear the idea and implement first logic.

--- src/MoveAllFiles.App/MoveAllFilesLogic.cs
+++ src/MoveAllFiles.App/MoveAllFilesLogic.cs
@@ -1,4 +1,6 @@
-﻿using System.IO.Abstractions;
+﻿using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MoveAllFiles.Tests")]
@@ -17,9 +19,22 @@ namespace MoveAllFiles.App
         {
             RootDirectoryPath = rootDirectoryPath;
 
-            // Search for all directories and work with atomic directories
-            // Move all files to the root directory then delete the empty dirctories
-            // Move temp files from root directory to the temp folder
+            // Search for all directorie paths
+            // Move all files to the root directory then delete the dirctories
+            // Move out of whitelist files from root directory to the temp folder
+        }
+
+        internal IEnumerable<string> GetAllDirectoryPaths(string rootDirectoryPath)
+        {
+            var paths = Enumerable.Empty<string>();
+            var directoryPaths = fileSystem.Directory.GetDirectories(rootDirectoryPath);
+            paths = paths.Union(directoryPaths);
+            foreach (var path in directoryPaths)
+            {
+                var subDirectoriesPath = GetAllDirectoryPaths(path);
+                paths = paths.Union(subDirectoriesPath);
+            }
+            return paths;
         }
     }
 }
--- src/MoveAllFiles.Tests/UnitTest1.cs
+++ src/MoveAllFiles.Tests/UnitTest1.cs
@@ -1,14 +1,35 @@
-using System;
+using MoveAllFiles.App;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using Xunit;
 
 namespace MoveAllFiles.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Fact(DisplayName ="System can list all directory paths")]
+        public void SystemCanListAllDirectoryPathsCorrectly()
         {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\myfile.txt", new MockFileData("Some text.") },
+                { @"c:\a\aQuery.js", new MockFileData("Some jquery") },
+                { @"c:\a\a1\aQuery.js", new MockFileData("Some jquery") },
+                { @"c:\b\aQuery.js", new MockFileData("Some jquery") },
+                { @"c:\b\b1\b12\aQuery.js", new MockFileData("Some jquery") },
+            });
 
+            var sut = new MoveAllFilesLogic(fileSystem);
+            var actual = sut.GetAllDirectoryPaths(@"c:\");
+            var expectedDirectories = new List<string>
+            {
+                @"c:\a\",
+                @"c:\b\",
+                @"c:\a\a1\",
+                @"c:\b\b1\",
+                @"c:\b\b1\b12\"
+            };
+            Assert.Equal(expectedDirectories, actual);
         }
     }
 }

ce1585ba118bac06bf52cfff6d11cd538d34eac4
sakul <miolynet@perfenterprise.com>
2018-09-21 08:58:05 +0700

    Complete scenario move files to root directory.

--- src/MoveAllFiles.App/MoveAllFilesLogic.cs
+++ src/MoveAllFiles.App/MoveAllFilesLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,22 +13,24 @@ namespace MoveAllFiles.App
         private readonly IFileSystem fileSystem;
 
         public string RootDirectoryPath { get; internal set; }
+        public IEnumerable<string> WhitelistExtensionNames { get; set; }
 
         public MoveAllFilesLogic(IFileSystem fileSystem) => this.fileSystem = fileSystem;
 
-        public void Begin(string rootDirectoryPath)
+        public void Begin(string rootDirectoryPath, IEnumerable<string> whitelistExtensionNames = null)
         {
             RootDirectoryPath = rootDirectoryPath;
+            WhitelistExtensionNames = whitelistExtensionNames ?? Enumerable.Empty<string>();
 
             // Search for all directorie paths
             // Move all files to the root directory then delete the dirctories
             // Move out of whitelist files from root directory to the temp folder
         }
 
-        internal IEnumerable<string> GetAllDirectoryPaths(string rootDirectoryPath)
+        internal IEnumerable<string> GetAllDirectoryPaths(string directoryPath)
         {
             var paths = Enumerable.Empty<string>();
-            var directoryPaths = fileSystem.Directory.GetDirectories(rootDirectoryPath);
+            var directoryPaths = fileSystem.Directory.GetDirectories(directoryPath);
             paths = paths.Union(directoryPaths);
             foreach (var path in directoryPaths)
             {
@@ -36,5 +39,24 @@ namespace MoveAllFiles.App
             }
             return paths;
         }
+
+        internal IEnumerable<string> GetAllFilePaths(string directoryPath) 
+            => fileSystem.Directory.GetFiles(directoryPath);
+
+        internal void MoveAllFilesToRootDirectoryAndDeleteIt(string workingDirectoryPath)
+        {
+            var filePaths = fileSystem.Directory.GetFiles(workingDirectoryPath);
+            foreach (var path in filePaths)
+            {
+                var fileInfo = fileSystem.FileInfo.FromFileName(path);
+                var destinationPath = $"{RootDirectoryPath}{fileInfo.Name}";
+                //var isFileExisting = fileSystem.File.Exists(destinationPath);
+                //if (isFileExisting) destinationPath = $"{RootDirectoryPath}{fileInfo.Name.Split(".", StringSplitOptions.RemoveEmptyEntries)[0]}{Guid.NewGuid().ToString().Split("-", StringSplitOptions.RemoveEmptyEntries)[0]}{fileInfo.Extension}";
+                fileSystem.File.Move(path, destinationPath);
+            }
+            
+fileSystem.Directory.Delete(workingDirectoryPath);
+        }
+
     }
 }
--- src/MoveAllFiles.Tests/UnitTest1.cs
+++ src/MoveAllFiles.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+using FluentAssertions.Collections;
 using MoveAllFiles.App;
 using System.Collections.Generic;
 using System.IO.Abstractions.TestingHelpers;
@@ -7,19 +9,17 @@ namespace MoveAllFiles.Tests
 {
     public class UnitTest1
     {
-        [Fact(DisplayName ="System can list all directory paths")]
+        [Fact(DisplayName = "System can list all directory paths.")]
         public void SystemCanListAllDirectoryPathsCorrectly()
         {
-            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
-            {
-                { @"c:\myfile.txt", new MockFileData("Some text.") },
-                { @"c:\a\aQuery.js", new MockFileData("Some jquery") },
-                { @"c:\a\a1\aQuery.js", new MockFileData("Some jquery") },
-                { @"c:\b\aQuery.js", new MockFileData("Some jquery") },
-                { @"c:\b\b1\b12\aQuery.js", new MockFileData("Some jquery") },
-            });
+            var fileSyste = new MockFileSystem();
+            fileSyste.AddFile(@"c:\myfile.txt", new MockFileData("Some text."));
+            fileSyste.AddFile(@"c:\a\aQuery.js", new MockFileData("Some jquery"));
+            fileSyste.AddFile(@"c:\a\a1\aQuery.js", new MockFileData("Some jquery"));
+            fileSyste.AddFile(@"c:\b\aQuery.js", new MockFileData("Some jquery"));
+            fileSyste.AddFile(@"c:\b\b1\b12\aQuery.js", new MockFileData("Some jquery"));
 
-            var sut = new MoveAllFilesLogic(fileSystem);
+            var sut = new MoveAllFilesLogic(fileSyste);
             var actual = sut.GetAllDirectoryPaths(@"c:\");
             var expectedDirectories = new List<string>
             {
@@ -30,6 +30,32 @@ namespace MoveAllFiles.Tests
                 @"c:\b\b1\b12\"
             };
             Assert.Equal(expectedDirectories, actual);
+        }
+
+        [Fact(DisplayName = "When we move files from a directory, System should delete that directory after that.")]
+        public void MoveAllFilesToRootDirectoryAndDeleteAllDirectories()
+        {
+            var fileSyste = new MockFileSystem();
+            fileSyste.AddFile(@"c:\a\aQuery.js", new MockFileData("Some jquery"));
+            fileSyste.AddFile(@"c:\a\aText.txt", new MockFileData("Some text."));
+
+            const string RootDirectoryPath = @"c:\";
+            var sut = new MoveAllFilesLogic(fileSyste);
+            sut.RootDirectoryPath = RootDirectoryPath;
+            sut.MoveAllFilesToRootDirectoryAndDeleteIt(@"c:\a\");
+
+            sut.GetAllDirectoryPaths(RootDirectoryPath)
+                .Should()
+                .BeEmpty("All directories must be deleted after we move their files.");
+
+            var expectedFilePaths = new[]
+            {
+                @"c:\aQuery.js",
+                @"c:\aText.txt",
+            };
+            sut.GetAllFilePaths(RootDirectoryPath)
+                .Should()
+                .BeEquivalentTo(expectedFilePaths, "All files must be here.");
         }
     }
 }
